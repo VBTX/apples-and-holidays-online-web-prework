91e7e4643d24e8b906b0e64b573ed63f0dd7366a
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 17:53:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,12 @@ end
 
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
+  holiday_hash.each do |season, holiday|
+    puts season.t_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      puts holiday.to_s.capitalize! << ":"
+      puts supplies.join(", ")
+  #iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
   #   New Years: Party Hats
