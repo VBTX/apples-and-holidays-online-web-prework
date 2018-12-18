0d08cca075681722089f63c6609768ce28b22c02
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 17:43:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].each do |key, value|
-    value.flatten
+  holiday_hash[:winter].values
+
 end
 end
 
