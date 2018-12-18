4489df8d54fa4735f04b79cfabf8509bf2dc280c
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 17:38:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring].each do |key, value|
-    value << "Grill"# again, holiday_hash is the same as the ones above
+    value << supply# again, holiday_hash is the same as the ones above
   end
 
 end
